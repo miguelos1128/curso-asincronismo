08de511b5b904e8e667b77f96212040abdfda516
Miguel Angel Osonio <miguelos1128@gmail.com>
2022-11-09 07:03:23 -0600

    creacion del metod fetchData para consultar la información

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,3 +1,19 @@
 const promise = new Promise(function (resolve, rejetct){
     resolve('Hey! todo correcto');
 });
+
+const cows = 15;
+
+const countCows = new Promise(function (resolve, reject) {
+    if (cows > 10){
+        resolve(`we have ${cows} cows ion the farm`);
+    }else{
+        reject('There is no cows on the farm');
+    }
+});
+
+countCows.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+}).finally(() => console.log('Finally'));
